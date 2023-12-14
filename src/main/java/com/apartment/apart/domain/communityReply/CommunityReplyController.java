d240d5ca722b9b10ce9f53575e6c87310d86187e
Jinyun <ppp6289@naver.com>
2023-12-14 12:17:53 +0900

    임시

--- src/main/java/com/apartment/apart/domain/communityReply/CommunityReplyController.java
+++ src/main/java/com/apartment/apart/domain/communityReply/CommunityReplyController.java
@@ -27,60 +27,60 @@ public class CommunityReplyController {
     private final CommunityReplyService communityReplyService;
     private final UserService userService;
 
-//    @PreAuthorize("isAuthenticated()")
-//    @PostMapping("/create/{id}")
-//    public String createCommunityReply(Model model, @PathVariable("id") Integer id,
-//                                       @Valid com.apartment.apart.communityReply.CommunityReplyForm communityReplyForm, BindingResult bindingResult, Principal principal) {
-//
-//        // 답변 부모 질문 객체를 받아온다.
-//        Community community = this.communityService.getCommunity(id);
-//        SiteUser siteUser = this.userService.getUser(principal.getName());
-//
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("community", community);
-//            return "community_detail";
-//        }
-//
-//        CommunityReply communityReply = this.communityReplyService.create(community, communityReplyForm.getContent(), siteUser);
-//
-//        return "redirect:/community/detail/%d".formatted(id);
-//    }
-//
-//    @PreAuthorize("isAuthenticated()")
-//    @GetMapping("/modify/{id}")
-//    public String communityReplyModify(com.apartment.apart.communityReply.CommunityReplyForm communityReplyForm, @PathVariable("id") Integer id, Principal principal) {
-//        CommunityReply communityReply = this.communityReplyService.getCommunityReply(id);
-//        if (!communityReply.getUser().getUsername().equals(principal.getName())) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
-//        }
-//        communityReplyForm = new com.apartment.apart.communityReply.CommunityReplyForm(communityReply.getContent());
-//
-//        return "community_reply_form";
-//    }
-//
-//    @PreAuthorize("isAuthenticated()")
-//    @PostMapping("/modify/{id}")
-//    public String communityReplyModify(@Valid com.apartment.apart.communityReply.CommunityReplyForm communityReplyForm, BindingResult bindingResult,
-//                                       @PathVariable("id") Integer id, Principal principal) {
-//        if (bindingResult.hasErrors()) {
-//            return "community_reply_form";
-//        }
-//        CommunityReply communityReply = this.communityReplyService.getCommunityReply(id);
-//        if (!communityReply.getUser().getUsername().equals(principal.getName())) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
-//        }
-//        this.communityReplyService.modify(communityReply, communityReplyForm.getContent());
-//        return String.format("redirect:/community/detail/%s", communityReply.getpost().getId());
-//    }
-//
-//    @PreAuthorize("isAuthenticated()")
-//    @GetMapping("/delete/{id}")
-//    public String communityReplyDelete(Principal principal, @PathVariable("id") Integer id) {
-//        CommunityReply communityReply = this.communityReplyService.getCommunityReply(id);
-//        if (!communityReply.getUser().getUsername().equals(principal.getName())) {
-//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
-//        }
-//        this.communityReplyService.delete(communityReply);
-//        return String.format("redirect:/community/detail/%s", communityReply.getpost().getId());
-//    }
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/create/{id}")
+    public String createCommunityReply(Model model, @PathVariable("id") Integer id,
+                                       @Valid com.apartment.apart.communityReply.CommunityReplyForm communityReplyForm, BindingResult bindingResult, Principal principal) {
+
+        // 답변 부모 질문 객체를 받아온다.
+        Community community = this.communityService.getCommunity(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("community", community);
+            return "community_detail";
+        }
+
+        CommunityReply communityReply = this.communityReplyService.create(community, communityReplyForm.getContent(), siteUser);
+
+        return "redirect:/community/detail/%d".formatted(id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String communityReplyModify(com.apartment.apart.communityReply.CommunityReplyForm communityReplyForm, @PathVariable("id") Integer id, Principal principal) {
+        CommunityReply communityReply = this.communityReplyService.getCommunityReply(id);
+        if (!communityReply.getUser().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        communityReplyForm = new com.apartment.apart.communityReply.CommunityReplyForm(communityReply.getContent());
+
+        return "community_reply_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String communityReplyModify(@Valid com.apartment.apart.communityReply.CommunityReplyForm communityReplyForm, BindingResult bindingResult,
+                                       @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "community_reply_form";
+        }
+        CommunityReply communityReply = this.communityReplyService.getCommunityReply(id);
+        if (!communityReply.getUser().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.communityReplyService.modify(communityReply, communityReplyForm.getContent());
+        return String.format("redirect:/community/detail/%s", communityReply.getCommunity().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String communityReplyDelete(Principal principal, @PathVariable("id") Integer id) {
+        CommunityReply communityReply = this.communityReplyService.getCommunityReply(id);
+        if (!communityReply.getUser().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.communityReplyService.delete(communityReply);
+        return String.format("redirect:/community/detail/%s", communityReply.getCommunity().getId());
+    }
 }
