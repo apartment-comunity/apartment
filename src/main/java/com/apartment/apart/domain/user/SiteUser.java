ae2bc6599e1895dd9dd3f8c2e7ee145b234c858b
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-01-03 11:00:47 +0900

    ajax적용

--- src/main/java/com/apartment/apart/domain/user/SiteUser.java
+++ src/main/java/com/apartment/apart/domain/user/SiteUser.java
@@ -1,10 +1,21 @@
 package com.apartment.apart.domain.user;
 
+import com.apartment.apart.domain.community.Community;
+import com.apartment.apart.domain.communityReply.CommunityReply;
+import com.apartment.apart.domain.report.Report;
+import com.apartment.apart.domain.reportReply.ReportReply;
+import com.apartment.apart.domain.schedule.Schedule;
+import com.apartment.apart.domain.voteTotal.VoteTotal;
 import com.apartment.apart.global.jpa.BaseEntity;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.Comment;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -33,4 +44,25 @@ public class SiteUser extends BaseEntity {
     private int apartHo;
 
     private boolean checkedAdmin;
+
+    @OneToMany(cascade = CascadeType.REMOVE)
+    private List<Report> reportList;
+
+    @OneToMany(cascade = CascadeType.REMOVE)
+    private List<ReportReply> reportReplyList;
+
+    @OneToMany(cascade = CascadeType.REMOVE)
+    private List<Schedule> scheduleList;
+
+    @OneToMany(cascade = CascadeType.REMOVE)
+    private List<VoteTotal> voteTotalList;
+
+    @OneToMany(cascade = CascadeType.REMOVE)
+    private List<Community> communityList;
+
+    @OneToMany(cascade = CascadeType.REMOVE)
+    private List<CommunityReply> communityReplyList;
+
+
+
 }
